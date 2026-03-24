1a3c685bf339b81b20763e89a7a94efc440b22e2
brandondionisio <brandon.dionisio@differenthealth.com>
2026-03-24 16:02:59 -0400

    wip dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
@@ -10,8 +11,8 @@ export default {
 			},
 			keyframes: {
 				pulsate: {
-					'0%, 100%': { transform: 'scale(1)' },
-					'50%': { transform: 'scale(1.10)' },
+					'0%, 100%': { transform: 'rotate(55deg) scale(1)' },
+					'50%': { transform: 'rotate(55deg) scale(1.1)' },
 				},
 			},
 			animation: {
