8dfcde41c605452ff9703a10242033c488eb78b2
brandondionisio <brandon.dionisio@differenthealth.com>
2026-05-16 14:38:44 -0400

    added ibm to hero

--- src/consts.ts
+++ src/consts.ts
@@ -1,13 +1,26 @@
-export const SITE_TITLE = 'Brandon Dionisio';
-export const BLOG_TITLE = 'Brandon Dionisio\'s Blog';
-export const PHOTOS_TITLE = 'Brandon Dionisio\'s Photos';
-export const SITE_DESCRIPTION = 'Welcome in! Here you can find some of my projects and blog posts.';
+export const SITE_TITLE = "Brandon Dionisio";
+export const BLOG_TITLE = "Brandon Dionisio's Blog";
+export const PHOTOS_TITLE = "Brandon Dionisio's Photos";
+export const SITE_DESCRIPTION =
+  "Welcome in! Here you can find some of my projects and blog posts.";
 
-export const AUTHOR_NAME = 'Brandon';
+export const AUTHOR_NAME = "Brandon";
 export const AUTHOR_BIO =
-	'I recently graduated from Tufts University with degrees in Computer Science and Cognitive Brain Science. I work at <a href="https://differenthealth.com" target="_blank" rel="noopener noreferrer">Different Health</a> and enjoy soccer, skiing, and playing word games like the <a href="https://minutecryptic.com" target="_blank" rel="noopener noreferrer">Minute Cryptic</a>, Word Hunt, and NYT Crossplay.';
+  'I recently graduated from Tufts University with degrees in Computer Science and Cognitive Brain Science. I work at <a href="https://ibm.com" target="_blank" rel="noopener noreferrer">Different Health</a> and will be joining <a href="https://differenthealth.com" target="_blank" rel="noopener noreferrer">IBM</a> this August. I enjoy playing soccer, skiing, and playing word games like the <a href="https://minutecryptic.com" target="_blank" rel="noopener noreferrer">Minute Cryptic</a>, Word Hunt, and NYT Crossplay.';
 export const SOCIAL_LINKS = [
-	{ name: 'LinkedIn', href: 'https://linkedin.com/in/brandondionisio', icon: 'linkedin' },
-	{ name: 'GitHub', href: 'https://github.com/brandondionisio', icon: 'github' },
-	{ name: 'Instagram', href: 'https://www.instagram.com/brandon.dionisio/', icon: 'instagram' },
+  {
+    name: "LinkedIn",
+    href: "https://linkedin.com/in/brandondionisio",
+    icon: "linkedin",
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/brandondionisio",
+    icon: "github",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/brandon.dionisio/",
+    icon: "instagram",
+  },
 ] as const;
